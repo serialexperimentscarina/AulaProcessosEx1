ff0c50155c72eafe41aef71365b8937ce637a979
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 16:44:10 -0300

    Add options

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.swing.JOptionPane;
+
 public class RedesController {
 
 		public RedesController() {
@@ -26,13 +28,14 @@ public class RedesController {
 				BufferedReader buffer = new BufferedReader(leitor);
 				String linha = buffer.readLine();
 				String adaptador = "";
+				StringBuffer resultado = new StringBuffer();
 				if (os().contains("Windows")) {
 					while (linha != null) {
 						if (linha.contains("Adaptador") || linha.contains("adapter")) {
 							adaptador = linha;
 						}
 						if (linha.contains("IPv4")) {
-							System.out.println(adaptador + linha.split(":")[1]);
+							resultado.append(adaptador + linha.split(":")[1] + "\n");
 						}
 						linha = buffer.readLine();
 					}
@@ -42,7 +45,7 @@ public class RedesController {
 							adaptador = linha.split(" ")[0];
 						}
 						if (linha.contains("inet ")) {
-							System.out.println(adaptador + " " + linha.split("inet ")[1].split(" ")[0]);
+							resultado.append(adaptador + " " + linha.split("inet ")[1].split(" ")[0] + "\n");
 						}
 						linha = buffer.readLine();
 					}
@@ -50,6 +53,7 @@ public class RedesController {
 				buffer.close();
 				leitor.close();
 				fluxo.close();
+				JOptionPane.showMessageDialog(null, resultado);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -65,6 +69,7 @@ public class RedesController {
 				BufferedReader buffer = new BufferedReader(leitor);
 				String linha = buffer.readLine();
 				String ultLinha = "";
+				JOptionPane.showMessageDialog(null, "Computando resultados... Por favor, clique em 'OK' e aguarde.");
 				while (linha != null) {
 					ultLinha = linha;
 					linha = buffer.readLine();
@@ -73,9 +78,9 @@ public class RedesController {
 				leitor.close();
 				fluxo.close();
 				if (os().contains("Windows")) {
-					System.out.println("\nMédia = " + ultLinha.split(" = ")[3]);
+					JOptionPane.showMessageDialog(null, "Média = " + ultLinha.split(" = ")[3]);
 				} else {
-					System.out.println("\nMédia = " + ultLinha.split("/")[4] + "ms");
+					JOptionPane.showMessageDialog(null, "Média = " + ultLinha.split("/")[4] + "ms");
 					
 				}
 			} catch (Exception e) {
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,13 +1,33 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		int opc;
 		RedesController redesController = new RedesController();
 		
+		do {
+			opc = Integer.parseInt(JOptionPane.showInputDialog("Menu : \n1 - Configuração de IP \n2- Chamada de ping em IPv4 \n3 - Finalizar"));
+			switch (opc) {
+				case 1:
+					redesController.ip();
+					break;
+				case 2:
+					redesController.ping();
+					break;
+				case 3: 
+					JOptionPane.showMessageDialog(null, "Encerrando execução.");
+					break;
+				default: 
+					JOptionPane.showMessageDialog(null, "Opção inválida!");
+					break;
+			}
+		} while (opc != 3);
+
 	}
 
 }
