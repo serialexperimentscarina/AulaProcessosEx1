8299e6fd3b7fd384d4edeadb9903e6a2137dd9e7
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 13:33:32 -0300

    Create "ip" method

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class RedesController {
 
 		public RedesController() {
@@ -10,5 +14,44 @@ public class RedesController {
 		private String os() {
 			String os = System.getProperty("os.name");
 			return os;
+		}
+		
+		// Verifica o Sistema Operacional e, de acordo com o S.O., faz a chamada de configuração de IP.
+		public void ip() {
+			String process = (os().contains("Windows")) ? "ipconfig" : "ifconfig";
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				String adaptador = "";
+				if (os().contains("Windows")) {
+					while (linha != null) {
+						if (linha.contains("Adaptador") || linha.contains("adapter")) {
+							adaptador = linha;
+						}
+						if (linha.contains("IPv4")) {
+							System.out.println(adaptador + linha.split(":")[1]);
+						}
+						linha = buffer.readLine();
+					}
+				} else {
+					while (linha != null) {
+						if (!linha.startsWith(" ")) {
+							adaptador = linha.split(" ")[0];
+						}
+						if (linha.contains("inet ")) {
+							System.out.println(adaptador + " " + linha.split("inet ")[1].split(" ")[0]);
+						}
+						linha = buffer.readLine();
+					}
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 }
