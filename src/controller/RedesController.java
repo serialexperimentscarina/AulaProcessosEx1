5ac91447076a2c712e6a54a6ffafaa6f2ffc8c83
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 10:03:47 -0300

    Create "os" method

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -5,4 +5,10 @@ public class RedesController {
 		public RedesController() {
 			super();
 		}
+		
+		// Identifica e retorna o nome do Sistema Operacional
+		private String os() {
+			String os = System.getProperty("os.name");
+			return os;
+		}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,13 @@
 package view;
 
+import controller.RedesController;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		RedesController redesController = new RedesController();
+		
 	}
 
 }
