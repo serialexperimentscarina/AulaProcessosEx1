f007c109f4a6a280ff0c938d4da0a37a359edb4d
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-02-26 22:53:45 -0300

    Change opc to String and add Exception handling

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,27 +7,29 @@ import controller.RedesController;
 public class Main {
 
 	public static void main(String[] args) {
-		int opc;
+		String opc = "";
 		RedesController redesController = new RedesController();
 		
-		do {
-			opc = Integer.parseInt(JOptionPane.showInputDialog("Menu : \n1 - Configuração de IP \n2- Chamada de ping em IPv4 \n3 - Finalizar"));
-			switch (opc) {
-				case 1:
-					redesController.ip();
-					break;
-				case 2:
-					redesController.ping();
-					break;
-				case 3: 
-					JOptionPane.showMessageDialog(null, "Encerrando execução.");
-					break;
-				default: 
-					JOptionPane.showMessageDialog(null, "Opção inválida!");
-					break;
-			}
-		} while (opc != 3);
-
+		try {
+			do {
+				opc = JOptionPane.showInputDialog("Menu : \n1 - Configuração de IP \n2- Chamada de ping em IPv4 \n3 - Finalizar");
+				switch (opc) {
+					case "1":
+						redesController.ip();
+						break;
+					case "2":
+						redesController.ping();
+						break;
+					case "3": 
+						JOptionPane.showMessageDialog(null, "Encerrando execução.");
+						break;
+					default: 
+						JOptionPane.showMessageDialog(null, "Opção inválida!");
+						break;
+					}
+			} while (!opc.equals("3"));
+		} catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Encerrando execução.");
+		}
 	}
-
 }
